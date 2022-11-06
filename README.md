f111e3f75959e4a9cb8236470d5e7d45f3662151
IsaccBarker <milobanks@rowlandhall.org>
2022-11-06 04:20:34 -0700

    KVM Support

---
+++ README.md
@@ -0,0 +1,3 @@
+# Kyttie
+Kytte is the Catnet reference implemention. More information will be added here... in the future!
+
---
+++ startvm.sh
@@ -0,0 +1,15 @@
+# qemu-system-x86_64 -display curses -kernel ./arch/x86/boot/bzImage -hda qemu-image.img -append root=/dev/sda single
+# qemu-system-x86_64 -display curses -kernel ./arch/x86/boot/bzImage -drive file=qemu-image.img,format=raw,index=0,media=disk -append root=/dev/sda single
+echo "Use 'shutdown -h now' for shutdown."
+echo "Please place your KVM combatable, built Linux source tree in the 'linux_source_tree.ignore' directory."
+echo "A raw QEMU image named 'qemu-image.img' should also be built there."
+qemu-system-x86_64 \
+    -display curses \
+    -kernel ./linux_source_tree.ignore/arch/x86/boot/bzImage \
+    -drive file=./linux_source_tree.ignore/qemu-image.img,format=raw,index=0,media=disk \
+    -append "root=/dev/sda loglevel=4 vga=6" \
+    -vga std \
+    --enable-kvm
+
+echo "Please proceed to the chamber lock."
+
