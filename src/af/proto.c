bddfeba38365aac80a694cb740318bdc0cb9a706
IsaccBarker <milobanks@rowlandhall.org>
2022-11-08 04:34:34 -0700

    Reformat

--- src/af/proto.c
+++ src/af/proto.c
@@ -2,10 +2,12 @@
 
 #include <net/sock.h>
 
-int register_cp_proto(void) {
+int register_cp_proto(void)
+{
     return proto_register(&cp_proto, 1);
 }
 
-void unregister_cp_proto(void) {
+void unregister_cp_proto(void)
+{
     proto_unregister(&cp_proto);
 }
