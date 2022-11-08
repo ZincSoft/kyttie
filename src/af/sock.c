adb6b91ff1b07dbd51d345c04a1040ccf5a12984
IsaccBarker <milobanks@rowlandhall.org>
2022-11-08 04:32:56 -0700

    Fix invalid opcode resulting from faulty proto_ops
    There used to be no assignments to the callbacks within the proto_ops
    struct, but when the kernel tried to call one it encountered garbage
    data resulting in a lock up/panic. It's fixed now!

--- src/af/sock.c
+++ src/af/sock.c
@@ -4,15 +4,18 @@
 #include "sock.h"
 #include "proto.h"
 
-int register_cp_sock(void) {
+int register_cp_sock(void)
+{
     return sock_register(&cp_family_ops);
 }
 
-void unregister_cp_sock(void) {
+void unregister_cp_sock(void)
+{
     sock_unregister(CP_PROTOCOL_FAMILY);
 }
 
-int cp_create(struct net* net, struct socket *sock, int proto, int kern) {
+int cp_create(struct net* net, struct socket *sock, int proto, int kern)
+{
     struct sock *sk;
     int rc;
 
@@ -24,7 +27,7 @@ int cp_create(struct net* net, struct socket *sock, int proto, int kern) {
         goto out;
     }
 
-    // Don't need to do anything more if they request a raw or connectionless
+    // Don't need to do anything more if they do not request a raw connectionless
     // socket.
     if (sock->type != SOCK_RAW && sock->type != SOCK_DGRAM)
         goto out;
@@ -35,10 +38,90 @@ int cp_create(struct net* net, struct socket *sock, int proto, int kern) {
     if (!kern && sock->type == SOCK_RAW && !capable(CAP_NET_RAW))
         goto out;
 
+    rc = -ENOMEM;
     sk = sk_alloc(net, CP_PROTOCOL_FAMILY, GFP_KERNEL, &cp_proto, kern);
     if (!sk)
         goto out;
 
+    rc = 0;
+
+    sock_init_data(sock, sk);
+
+    sock->ops = &cp_raw_ops;
+    sock->state = SS_UNCONNECTED;
+
     out:
         return rc;
 }
+
+/*
+ * THIS
+ *      IS WHERE THE
+ *                   MAGIC HAPPENS!
+ */
+
+int cp_raw_release(struct socket *)
+{
+    return -EAFNOSUPPORT;
+}
+
+int cp_raw_bind(struct socket *, struct sockaddr *, int)
+{
+    return -EAFNOSUPPORT;
+}
+
+int cp_raw_connect(struct socket *, struct sockaddr *, int, int)
+{
+    return -EAFNOSUPPORT;
+}
+
+int cp_raw_accept(struct socket *, struct socket *, int, _Bool)
+{
+    return -EAFNOSUPPORT;
+}
+
+int cp_raw_getname(struct socket *, struct sockaddr *, int)
+{
+    return -EAFNOSUPPORT;
+}
+
+__poll_t cp_raw_poll(struct file *, struct socket *, struct poll_table_struct *)
+{
+    return 0;
+}
+
+int cp_raw_ioctl(struct socket *, unsigned int, long unsigned int)
+{
+    return -EAFNOSUPPORT;
+}
+
+int cp_raw_listen(struct socket *, int)
+{
+    return -EAFNOSUPPORT;
+}
+
+int cp_raw_shutdown(struct socket *, int)
+{
+    return -EAFNOSUPPORT;
+}
+
+int cp_raw_setsockopt(struct socket *, int, int, sockptr_t, unsigned int)
+{
+    return -EAFNOSUPPORT;
+}
+
+int cp_raw_getsockopt(struct socket *, int, int, char *, int *)
+{
+    return -EAFNOSUPPORT;
+}
+
+int cp_raw_sendmsg(struct socket *, struct msghdr *, size_t)
+{
+    return -EAFNOSUPPORT;
+}
+
+int cp_raw_recvmsg(struct socket *, struct msghdr *, size_t, int)
+{
+    return -EAFNOSUPPORT;
+}
+
--- src/af/sock.h
+++ src/af/sock.h
@@ -6,7 +6,29 @@
 
 #define CP_PROTOCOL_FAMILY PF_CP
 
+/** Registers the Catnet socket functionality with the kernel.*/
+int register_cp_sock(void);
+
+/** Unregisters the Catnet socket functionality from the kernel. */
+void unregister_cp_sock(void);
+
+/** Create socket handler. */
 int cp_create(struct net* net, struct socket *sock, int proto, int kern);
+
+/** Raw Catnet socket operations. */
+int cp_raw_release(struct socket *);
+int cp_raw_bind(struct socket *, struct sockaddr *, int);
+int cp_raw_connect(struct socket *, struct sockaddr *, int, int);
+int cp_raw_accept(struct socket *, struct socket *, int, _Bool);
+int cp_raw_getname(struct socket *, struct sockaddr *, int);
+__poll_t cp_raw_poll(struct file *, struct socket *, struct poll_table_struct *);
+int cp_raw_ioctl(struct socket *, unsigned int, long unsigned int);
+int cp_raw_listen(struct socket *, int);
+int cp_raw_shutdown(struct socket *, int);
+int cp_raw_setsockopt(struct socket *, int, int, sockptr_t, unsigned int);
+int cp_raw_getsockopt(struct socket *, int, int, char *, int *);
+int cp_raw_sendmsg(struct socket *, struct msghdr *, size_t);
+int cp_raw_recvmsg(struct socket *, struct msghdr *, size_t, int);
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-variable"
@@ -23,15 +45,26 @@ static const struct net_proto_family cp_family_ops = {
     .create = cp_create,
     .owner  = THIS_MODULE,
 };
+
+/** Describe Catnet raw protocol family. */
+static const struct proto_ops cp_raw_ops = {
+    .family = CP_PROTOCOL_FAMILY,
+    .owner = THIS_MODULE,
+
+    .release    = cp_raw_release,
+    .bind       = cp_raw_bind,
+    .connect    = cp_raw_connect,
+    .accept     = cp_raw_accept,
+    .getname    = cp_raw_getname,
+    .poll       = cp_raw_poll,
+    .ioctl      = cp_raw_ioctl,
+    .listen     = cp_raw_listen,
+    .shutdown   = cp_raw_shutdown,
+    .setsockopt = cp_raw_setsockopt,
+    .getsockopt = cp_raw_getsockopt,
+    .sendmsg    = cp_raw_sendmsg,
+    .recvmsg    = cp_raw_recvmsg,
+};
 #pragma GCC diagnostic pop
-
-/** Registers the Catnet socket functionality with the kernel.*/
-int register_cp_sock(void);
-
-/** Unregisters the Catnet socket functionality from the kernel. */
-void unregister_cp_sock(void);
-
-/** Create socket handler. */
-int cp_create(struct net* net, struct socket *sock, int proto, int kern);
 
 #endif /* _CP_SOCK_H_ */
