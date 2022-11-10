f80da0690e63cbfa2a53892446f0b245ef7f2dd4
IsaccBarker <milobanks@rowlandhall.org>
2022-11-09 23:52:33 -0700

    Build for native kernel as default

--- kinvoke.mk
+++ kinvoke.mk
@@ -1,7 +1,6 @@
 $(_UNIT_NAME)_OBJS := $(addsuffix $(_OBJEXT),$(addprefix $($(_UNIT_NAME)_OUTPUT)/,$(basename $(SRCS))))
 $(_UNIT_NAME)_KMOD := $($(_UNIT_NAME)_OUTPUT)/$(KMOD)$(KMOD_EXT)
-# KDIR := /lib/modules/$(shell uname -r)/build
-KDIR := linux_source_tree.ignore
+KDIR := /lib/modules/$(shell uname -r)/build
 
 ifneq ($(_NO_RULES),T)
 ifneq ($($(_UNIT_NAME)_DEFINED),T)
